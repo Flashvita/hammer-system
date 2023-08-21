156cd83eb06856129cc6d48b7b048835797dd278
Vitaliy Kuleshov <vitalijkulesov975@gmail.com>
2023-08-21 21:11:00 +0300

    add host

--- app/settings.py
+++ app/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '81.163.30.19']
 
 
 # Application definition
